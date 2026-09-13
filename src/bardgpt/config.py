90cb85dde9c17ceb439ed3ea8908f49a8a5fd412
Bardia Emamgholizadeh <bardia.egzz@gmail.com>
2026-09-13 22:02:00 +0330

    BardGPT can train on tiny shakespear dataset. yay

--- src/bardgpt/config.py
+++ src/bardgpt/config.py
@@ -1,5 +1,16 @@
 from dataclasses import dataclass
 
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    OKGREEN = '\033[38;2;123;198;33m'
+
 @dataclass
 class GPTConfig:
     vocab_size: int = 50_257
@@ -14,3 +25,4 @@ T = 32
 k = 50
 max_length = 30
 num_return_sequenecs = 5
+max_step = 500
--- src/bardgpt/train.py
+++ src/bardgpt/train.py
@@ -1,10 +1,11 @@
 from .model import GPT
 from .config import *
+from .data import DataLoader
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import tiktoken
-
+from tqdm import tqdm
 
 def main() -> None:
     enc = tiktoken.get_encoding('gpt2')
@@ -16,17 +17,33 @@ def main() -> None:
     model = GPT(GPTConfig(vocab_size=50_304))
     model.to('mps') # TODO: add auto-detect device
 
-    model.eval()
-    with torch.inference_mode():
-        while prompt.size(1) < max_length:
-            logits, _ = model(prompt)
-            logits = logits[:, -1, :]
-            probs = F.softmax(input=logits, dim=-1)
-            topk_probs, topk_indicies = torch.topk(input=probs, k=k, dim=-1)
-            ix = torch.multinomial(input=topk_probs, num_samples=1)
-            xcol = torch.gather(input=topk_indicies, dim=-1, index=ix)
-            prompt = torch.cat((prompt, xcol), dim=1)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=6e-4)
+    train_loader = DataLoader(B=B, T=T, device='mps')
 
-    for i in range(num_return_sequenecs):
-        decoded = enc.decode(prompt[i, :max_length].tolist())
-        print(f'> {decoded}')
+    pbar = tqdm(range(max_step), desc='Training BardGPT', colour='#7BC621', dynamic_ncols=True)
+    for step in pbar:
+        x_gen = prompt.clone()
+        last_step = (step == max_step - 1)
+        if (step > 0 and step % 100 == 0) or last_step:
+            model.eval()
+            with torch.inference_mode():
+                while x_gen.size(1) < max_length:
+                    logits, _ = model(x_gen)
+                    logits = logits[:, -1, :]
+                    probs = F.softmax(input=logits, dim=-1)
+                    topk_probs, topk_indicies = torch.topk(input=probs, k=k, dim=-1)
+                    ix = torch.multinomial(input=topk_probs, num_samples=1)
+                    xcol = torch.gather(input=topk_indicies, dim=-1, index=ix)
+                    x_gen = torch.cat((x_gen, xcol), dim=1)
+            for i in range(num_return_sequenecs):
+                decoded = enc.decode(x_gen[i, :max_length].tolist())
+                print(f'SAMPLE {i} -> {colors.OKGREEN}{decoded}{colors.ENDC}\n')
+            model.train()
+        x, y = train_loader.next_batch()
+        logits, loss = model(x, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        pbar.set_postfix(loss=f'{loss.item():.6f}')
+
+
